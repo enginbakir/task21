7559acb4b4e3c33463b5005a09fa1f420e04c48d
enginbakir <enginbakir1311@gmail.com>
2022-03-20 12:30:52 +0300

    Scheduled Commit - every 15 minutes

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -14,11 +14,21 @@ class Task extends Model
     const DONE = 3;
 
     public $assignedUser;
-
     protected $fillable = ['title', 'description', 'status', 'user_id'];
+
+    public function __construct(array $attributes = [])
+    {
+        $this->assignedUser = User::factory()->create();
+        parent::__construct($attributes);
+    }
 
     public function assignedUser()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function sortByStatus()
+    {
+        $task = Task::all();
     }
 }
