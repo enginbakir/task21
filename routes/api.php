7ab268848b9f947504bf01768861fd292d8b7902
enginbakir <enginbakir1311@gmail.com>
2022-03-20 11:05:53 +0300

    Scheduled Commit - every 15 minutes

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
 Route::get('/tasks/{id}', [TaskController::class, 'getOne']);
-Route::get('/tasks/{id}', [TaskController::class, 'getOne']);
-
+Route::get('/tasks', [TaskController::class, 'getAll']);
+Route::post('/tasks', [TaskController::class, 'create']);
+Route::delete('/tasks/{id}', [TaskController::class, 'remove']);
+Route::patch('/tasks/{id}', [TaskController::class, 'update']);
