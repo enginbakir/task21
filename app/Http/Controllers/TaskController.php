79449ae4c4d81d6e0ef8cf9688549d24336f7b10
enginbakir <enginbakir1311@gmail.com>
2022-03-20 11:15:53 +0300

    Scheduled Commit - every 15 minutes

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,8 +4,33 @@ namespace App\Http\Controllers;
 
 class TaskController
 {
-    public function getOne(){
-        echo "hello one";
+    public function getOne()
+    {
+        echo "hello getOne";
+        exit;
+    }
+
+    public function getAll()
+    {
+        echo "hello getAll";
+        exit;
+    }
+
+    public function create()
+    {
+        echo "hello create";
+        exit;
+    }
+
+    public function remove()
+    {
+        echo "hello remove";
+        exit;
+    }
+
+    public function update()
+    {
+        echo "hello update";
         exit;
     }
 }
